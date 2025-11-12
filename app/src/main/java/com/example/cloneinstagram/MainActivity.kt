570dd053249525a5a52650e99f356324ca95af5c
Phsithzz <phoosith2548@gmail.com>
2025-11-12 14:59:49 +0700

    BottomBar

--- app/src/main/java/com/example/cloneinstagram/MainActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,13 +14,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,13 +50,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CloneinstagramTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { paddings ->
+                Scaffold(
+                    topBar = {
+                        TopBar()
+                    },
+                    bottomBar = {
+                        BottomBar()
+                    }
+                ) { paddings ->
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(paddings)
                     ) {
-                        TopBar()
+
 
                     }
                 }
@@ -63,6 +76,7 @@ class MainActivity : ComponentActivity() {
 fun TopBar() {
     Row(modifier = Modifier
         .fillMaxWidth()
+        .statusBarsPadding()
         .padding(horizontal = 14.dp, vertical = 10.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
@@ -88,13 +102,39 @@ fun TopBar() {
         }
     }
 }
+data class BottomBarItem(
+    val iconRes: Int,
+    val description: String
+)
 
+val bottomBarItems = listOf(
+    BottomBarItem(R.drawable.ic_home, "Home"),
+    BottomBarItem(R.drawable.ic_search, "Search"),
+    BottomBarItem(R.drawable.ic_add, "Add"),
+    BottomBarItem(R.drawable.ic_ig, "Reels")
+)
 @Composable
-fun StoryList(){
+fun BottomBar(){
     Row(modifier = Modifier
         .fillMaxWidth()
-        .padding(horizontal = 10.dp, vertical = 10.dp)
+
+        .padding(vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
     ){
+        bottomBarItems.forEach{ item ->
+            Icon(
+                modifier = Modifier.size(28.dp),
+                painter = painterResource(id = item.iconRes),
+                contentDescription = item.description
+            )
+        }
+        Image(
+            modifier = Modifier.size(30.dp).clip(CircleShape),
+            painter = painterResource(id=R.drawable.profile),
+            contentDescription = "User Profile",
+            contentScale = ContentScale.Crop
+        )
 
     }
 }
